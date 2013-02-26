a8a0e22e2987ef76adca0405668deb4b322c7c8d
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2013-02-26 02:34:15 +0200

    set emacs 24.3 dependency for elpa

--- ph-pkg.el
+++ ph-pkg.el
@@ -1,1 +1,1 @@
-(define-package "ph" "0.0.1" "A global minor mode for managing multiple projects." (quote nil))
+(define-package "ph" "0.0.1" "A global minor mode for managing multiple projects." (quote ((emacs "24.3"))))
